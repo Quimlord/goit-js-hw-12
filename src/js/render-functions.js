c5e2f4fbe5c520052ce7dc0e771ff2814a834481
Yaroslav <116573793+Quimlord@users.noreply.github.com>
2025-03-03 22:22:17 +0200

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,19 +2,16 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader-box');
 const btnLoadMore = document.getElementById('load-more');
-
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
-
 export function renderImages(images, isNextPage = false) {
+  hideLoadingView();
   if (!isNextPage) gallery.innerHTML = '';
-
   const galleryHtml = images
     .map(
       ({
@@ -25,8 +22,8 @@ export function renderImages(images, isNextPage = false) {
         views,
         comments,
         downloads,
-      }) => `
-          <li class="gallery-item">
+      }) =>
+          `<li class="gallery-item">
             <a class="gallery-link" href="${largeImageURL}">
               <figure class="thumb-container">
                 <img
@@ -57,43 +54,36 @@ export function renderImages(images, isNextPage = false) {
                 </figcaption>
               </figure>
             </a>
-          </li>
-        `
+          </li>`
     )
     .join('');
-
   gallery.insertAdjacentHTML('beforeend', galleryHtml);
   lightbox.refresh();
-
   if (isNextPage && images.length) {
     scrollGallery();
   }
 }
-
 function scrollGallery() {
   const card = document.querySelector('.gallery-item');
   if (!card) return;
-
   const cardHeight = card.getBoundingClientRect().height;
   window.scrollBy({
     top: cardHeight * 2,
     behavior: 'smooth',
   });
 }
-
 export function showLoadingView(isNextPage = false) {
   if (!isNextPage) {
     gallery.classList.add('hidden');
   }
   loader.classList.remove('hidden');
 }
-
 export function hideLoadingView() {
   gallery.classList.remove('hidden');
   loader.classList.add('hidden');
 }
-
 export function showMessageNoResults() {
+  hideLoadingView();
   iziToast.error({
     position: 'topRight',
     title: 'Error',
@@ -109,11 +99,17 @@ export function showMessageLastPage() {
     message: "We're sorry, but you've reached the end of search results.",
   });
 }
-
+export function showMessageError() {
+  hideLoadingView();
+  iziToast.error({
+    position: 'topRight',
+    title: 'Error',
+    message: 'An error occurred while loading images. Please try again.',
+  });
+}
 export function showButtonLoadMore() {
   btnLoadMore.classList.remove('hidden');
 }
-
 export function hideButtonLoadMore() {
   btnLoadMore.classList.add('hidden');
 }
